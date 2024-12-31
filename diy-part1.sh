402987bef126962ff8b88c9c692b167814a02eb1
Lowrie <101512029+LittleLowrie@users.noreply.github.com>
2024-12-31 20:50:22 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -18,5 +18,4 @@
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 
 # add theme
-cd package
-git clone https://github.com/jerrykuku/luci-theme-argon.git
+
